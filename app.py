a64dcc45377929424fbbec3de7ae7536763054cb
Nehal Patel <iamnehalpatel@gmail.com>
2025-10-07 16:13:19 +0530

    new changes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import os
 from pyapp.main import app
 
-# This file is needed for Render to find the FastAPI app
-# when using gunicorn as the WSGI server
+# This file provides the app object for Render deployment
+# The Procfile will use uvicorn to run the FastAPI app
 
 if __name__ == "__main__":
     import uvicorn
+    import os
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
